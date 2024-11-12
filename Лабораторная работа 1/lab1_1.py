8f161d45a1a5c9955bca30bb23a28d2424263085
raindancerr <raindancerr@yahoo.com>
2024-11-13 02:27:23 +0300

    Add files via upload

---
+++ Лабораторная работа 1/lab1_1.py
@@ -0,0 +1,9 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+numbers[4]=0
+totalv=sum(numbers)
+ccount=len(numbers)
+avg=totalv/ccount
+numbers[4]=avg
+print("Измененный список:", numbers)
